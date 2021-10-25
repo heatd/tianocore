3f30c783620598b3637ce632a27f3de1044cc6fc
Pedro Falcato <pedro.falcato@gmail.com>
2021-10-25 23:15:46 +0100

    edksetup.bat: Switch to Python 3.10
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- edksetup.bat
+++ edksetup.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set PYTHON_HOME=C:\Python39
+set PYTHON_HOME=C:\Python310
 set WORKSPACE=%cd%
 set PACKAGES_PATH=%WORKSPACE%\edk2;%WORKSPACE%\edk2-platforms
 set NASM_PREFIX=C:\Program Files\NASM\
